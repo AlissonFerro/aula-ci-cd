56819f0530e19ea0629d8bc4994f94f90a2a94c0
Alisson Ferro <92187207+AlissonFerro@users.noreply.github.com>
2024-04-22 19:21:39 -0300

    feat: add test suite

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -32,38 +32,38 @@ describe('GET /user', () => {
     });
 });
 
-// describe('GET by ID', () => {
-//     beforeEach(() => {
-//         User.findById.mockResolvedValue(
-//             { _id: '2', name: 'Alisson', lastname: 'Ferro', salary: 1234 }
-//         );
-//     });
-//     afterEach(async () => {
-//         jest.clearAllMocks();
-//     });
-//     it('Deve retornar status 400', async () => {
-//         const res = await request(app).get('/api/user/2');
-//         expect(res.status).toBe(200);
-//     });
-// });
+describe('GET by ID', () => {
+    beforeEach(() => {
+        User.findById.mockResolvedValue(
+            { _id: '2', name: 'Alisson', lastname: 'Ferro', salary: 1234 }
+        );
+    });
+    afterEach(async () => {
+        jest.clearAllMocks();
+    });
+    it('Deve retornar status 400', async () => {
+        const res = await request(app).get('/api/user/2');
+        expect(res.status).toBe(200);
+    });
+});
 
-// describe('POST', ()=> {
-//     beforeEach(() => {
-//         User.create.mockResolvedValue([
-//             { _id: '1', name: 'Queila', lastname: 'Lima', salary: 1234 },
-//         ]);
-//     });
-//     afterEach(() => {
-//         jest.clearAllMocks();
-//     });
-//     it('Deve enviar os dados e salvar no banco', async () => {
-//         const userData = { name: 'Queila', lastname: 'Lima', salary: 1234 };
-//         const res = await request(app).post('/api/user').send(userData);
+describe('POST', ()=> {
+    beforeEach(() => {
+        User.create.mockResolvedValue([
+            { _id: '1', name: 'Queila', lastname: 'Lima', salary: 1234 },
+        ]);
+    });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    it('Deve enviar os dados e salvar no banco', async () => {
+        const userData = { name: 'Queila', lastname: 'Lima', salary: 1234 };
+        const res = await request(app).post('/api/user').send(userData);
 
-//         expect(res.status).toBe(201);
-//         expect(res.body).toHaveProperty('name', userData.name);
-//         expect(res.body).toHaveProperty('lastname', userData.lastname);
-//         expect(res.body).toHaveProperty('salary', userData.salary);
-//         expect(User.create).toHaveBeenCalledWith(userData);
-//     });
-// });
+        expect(res.status).toBe(201);
+        expect(res.body).toHaveProperty('name', userData.name);
+        expect(res.body).toHaveProperty('lastname', userData.lastname);
+        expect(res.body).toHaveProperty('salary', userData.salary);
+        expect(User.create).toHaveBeenCalledWith(userData);
+    });
+});
