a779fff3e74b5c90a90546912bb911cf4eff665c
Alisson <alisson_.ferro@hotmail.com>
2024-04-22 11:38:55 -0300

    fix: change config tests

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const config = require('config')
 module.exports = function(){
     const db = config.get('db');
 
-    mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(db)
     .then(() => console.log(`connected to ${db}`));
     
     if (process.env.NODE_ENV === 'test') {
