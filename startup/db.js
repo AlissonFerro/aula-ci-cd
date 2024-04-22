d3297ec42ebe2c3170e0c693bc9a5536ed0a9617
Alisson <alisson_.ferro@hotmail.com>
2024-04-22 11:07:53 -0300

    fix: change config deployment

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,12 @@ const config = require('config')
  
 module.exports = function(){
     const db = config.get('db');
+    
+    if (process.env.NODE_ENV === 'test') {
+        mongoose.connection.close(function () {
+          console.log('Mongoose connection disconnected');
+        });
+      }
     mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log(`connected to ${db}`));
 }
