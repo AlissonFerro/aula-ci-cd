2a5696d758f9a44455acd37e5d9148cc0148e392
Alisson Ferro <92187207+AlissonFerro@users.noreply.github.com>
2024-04-22 18:53:17 -0300

    fix: change config

--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../../index');
 const User = require('../../models/User');
-const db = require('../../startup/db');
+const mongoose = require('mongoose');
 
 jest.mock('../../models/User', () => ({
     create: jest.fn(),
@@ -23,7 +23,7 @@ describe('GET /user', () => {
     });
     afterAll(async () => {
         app.close();
-        await db.connection.close();
+        await mongoose.connection.close();
     }, 30000);
     it('Deve retornar um array com 3 elementos', async () => {
         const res = await request(app).get('/api/user');
